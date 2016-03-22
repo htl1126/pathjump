716cfc9e9f770222e73dd6cba3c553e53ee7c73e
Tian-Liang Huang <th574@cornell.edu>
2016-03-22 15:13:44 -0400

    add STATICFILES_DIRS to backend/setting.py note: the name of the static files directory is subject to the host machine

--- backend/settings.py
+++ backend/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    '/home/htl/Desktop/cornell_tech/startup_studio/pathjump/static'
+]
