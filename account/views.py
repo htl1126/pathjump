eb0a3dd85ed06b1eb5c180e1c6a3ea8a59e1c219
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-04-04 15:40:49 -0400

    add a page to get an user to login page when registered successfully

--- account/views.py
+++ account/views.py
@@ -12,7 +12,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            return HttpResponseRedirect('/')
+            return render(request, 'account/reg_success.html')
     else:
         form = UserCreationForm()
         return render(request, 'account/register.html', {'form': form})
