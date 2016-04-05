5455d0429b4efce1c531f27172093a333c96fe66
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-04-05 10:32:32 -0400

    update the database

--- account/views.py
+++ account/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponseRedirect
+from .forms import UserForm, UserProfileForm
 
 
 def profile(request):
@@ -8,11 +7,26 @@ def profile(request):
 
 
 def register(request):
+    registered = False
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            return render(request, 'account/reg_success.html')
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            registered = True
+        else:
+            print user_form.errors, profile_form.errors
     else:
-        form = UserCreationForm()
-        return render(request, 'account/register.html', {'form': form})
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render(request, 'account/register.html',
+                  {'user_form': user_form, 'profile_form': profile_form,
+                   'registered': registered})
