46572b88eb9ccf50929749a2a1ed60557d224517
Ubuntu <ubuntu@ip-172-31-27-221.us-west-2.compute.internal>
2016-04-07 22:50:38 +0000

    update backend/wsgi.py to support server runnning on apache

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/ubuntu/pathjump/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
