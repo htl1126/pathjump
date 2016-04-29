d6a7fce55602ce2b0a7ee3c4f2868f30d3ad759a
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-04-29 12:36:14 -0400

    add job app

---
+++ job/models.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class JobPost(models.Model):
+    title = models.CharField(max_length=50)
+    company = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    industry = models.CharField(max_length=50, blank=True)
+    employment_type = models.CharField(max_length=50, blank=True)
+    experience = models.CharField(max_length=50, blank=True)
+    job_function = models.CharField(max_length=50, blank=True)
+    company_logo = models.ImageField(upload_to='company_logo')
+
+    def __unicode__(self):
+        return self.title
