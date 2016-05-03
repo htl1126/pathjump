19cb90c5a2777f5257536b5f467051a9b046bf56
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-05-03 13:30:27 -0400

    job board available at /job/board and showing post available

--- job/urls.py
+++ job/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^post/', views.post, name='post'),
+    url(r'^board/(?P<page_number>[0-9]+)', views.job_board,
+        name='job_board'),
+    url(r'^board/', views.job_board,
+        name='job_board'),
+    url(r'^show_post/(?P<job_id>[0-9]+)', views.show_post,
+        name='show_post'),
 ]
--- job/views.py
+++ job/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import JobPostForm
+from .models import JobPost
 
 
 def post(request):
@@ -18,3 +19,34 @@ def post(request):
         job_form = JobPostForm()
     return render(request, 'job/post.html',
                   {'job_form': job_form, 'post_success': post_success})
+
+
+def job_board(request, page_number=None):
+    if not page_number:
+        page_number = 1
+    else:
+        page_number = int(page_number)
+    page_size = 3  # at most 3 jobposts in a page, hardcoded so far
+    job_post_size = JobPost.objects.count()
+    min_page_number = 1
+    max_page_number = job_post_size / page_size
+    if job_post_size % 3 != 0:
+        max_page_number += 1
+    page_number_list = range(min_page_number, max_page_number + 1)
+    first_post_in_page = (page_number - 1) * page_size
+    if first_post_in_page > job_post_size:
+        page_number = 1
+    if page_number * page_size > job_post_size:
+        job_post_list = JobPost.objects.order_by('pk')[
+            first_post_in_page:job_post_size]
+    else:
+        job_post_list = JobPost.objects.order_by('pk')[
+            first_post_in_page:page_number * page_size]
+    return render(request, 'job/board.html',
+                  {'job_list': job_post_list,
+                   'page_number_list': page_number_list})
+
+
+def show_post(request, job_id):
+    job_post = JobPost.objects.get(pk=int(job_id))
+    return render(request, 'job/show_post.html', {'job_post': job_post})
