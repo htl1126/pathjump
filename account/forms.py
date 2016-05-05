483496dac24b6a47051f75e5332d8e8ad49fff50
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-05-05 18:45:13 -0400

    modify model in account/

--- account/forms.py
+++ account/forms.py
@@ -12,22 +12,14 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
-    birthday = forms.DateField(widget=forms.SelectDateWidget(
-        years=range(1980, 2016)))
-    university_grad_date_1 = forms.DateField(widget=forms.SelectDateWidget(
-        years=range(1980, 2016)))
-    university_grad_date_2 = forms.DateField(widget=forms.SelectDateWidget(
-        years=range(1980, 2016)))
-    university_grad_date_3 = forms.DateField(widget=forms.SelectDateWidget(
-        years=range(1980, 2016)))
 
     class Meta:
         model = UserProfile
-        fields = ('picture', 'university_grad_date_1', 'major_1', 'gpa_1',
-                  'university_grad_date_2', 'major_2', 'gpa_2',
-                  'university_grad_date_3', 'major_3', 'gpa_3',
+        fields = ('picture', 'major_1', 'gpa_1',
+                  'major_2', 'gpa_2',
+                  'major_3', 'gpa_3',
                   'company_1', 'job_title_1', 'job_desc_1',
                   'company_2', 'job_title_2', 'job_desc_2',
                   'company_3', 'job_title_3', 'job_desc_3',
-                  'about_me', 'fun_fact', 'skill', 'birthday', 'country',
+                  'about_me', 'fun_fact', 'skill', 'country',
                   'city', 'address', 'zipcode', 'user_val')
